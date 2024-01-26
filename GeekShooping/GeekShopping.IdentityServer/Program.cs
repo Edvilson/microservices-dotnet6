772eeb0655748edb070a8fe785ac045a73718a7e
edvilson <edvilson.ads@gmail.com>
2024-01-25 22:35:06 -0300

    Create DB and Add Migrations Identity Server

--- GeekShooping/GeekShopping.IdentityServer/Program.cs
+++ GeekShooping/GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,11 @@
+using Duende.IdentityServer.Services;
 using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Initializer;
 using GeekShopping.IdentityServer.Model.Context;
+using GeekShopping.IdentityServer.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace GeekShopping.IdentityServer
 {
@@ -11,30 +15,41 @@ namespace GeekShopping.IdentityServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
+            var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
 
-            builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 1, 0))));
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<MySQLContext>()
+            builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+                connection,
+                new MySqlServerVersion(new Version(8, 0, 29)))
+            );
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MySQLContext>()
                 .AddDefaultTokenProviders();
 
-            var _builder = builder.Services.AddIdentityServer(options =>
+            var builderServices = builder.Services.AddIdentityServer(options =>
             {
                 options.Events.RaiseErrorEvents = true;
                 options.Events.RaiseInformationEvents = true;
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
                 options.EmitStaticAudienceClaim = true;
-            }).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
-            .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
-            .AddInMemoryClients(IdentityConfiguration.Clients)
-            .AddAspNetIdentity<ApplicationUser>();
+            })
+                .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+                .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                .AddInMemoryClients(IdentityConfiguration.Clients)
+                .AddAspNetIdentity<ApplicationUser>();
 
-            _builder.AddDeveloperSigningCredential();
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+            builder.Services.AddScoped<IProfileService, ProfileService>();
+
+            builderServices.AddDeveloperSigningCredential();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            var initializer = app.Services.CreateScope().ServiceProvider.GetService<IDbInitializer>();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -42,14 +57,18 @@ namespace GeekShopping.IdentityServer
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            }            
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+
             app.UseIdentityServer();
+
             app.UseAuthorization();
+
+            initializer.Initialize();
 
             app.MapControllerRoute(
                 name: "default",
